835621ba2dcb686f13f6800a9d0518c39a2811e7
AbdelrehmanElhaj <hdr333@gmail.com>
2025-03-09 15:19:15 +0000

    all entities completed and RestaurantRepository as well

---
+++ src/main/java/dev/hdr/restaurant_review/domain/repositories/RestaurantRepository.java
@@ -0,0 +1,11 @@
+package dev.hdr.restaurant_review.domain.repositories;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+import dev.hdr.restaurant_review.domain.entities.Restaurant;
+
+@Repository
+public interface RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {
+    // TODO Custom queries
+}
